ab19aab1d06dec559091d1b019c2557cbbafaf5a
nguyentienvu4897 <nguyentienvu4897@gmail.com>
2025-04-26 17:27:10 +0700

    demua

--- resources/views/site/partials/filter_product.blade.php
+++ resources/views/site/partials/filter_product.blade.php
@@ -1,6 +1,6 @@
 @foreach ($products as $product)
     @include('site.products.product_item', ['product' => $product])
 @endforeach
-<div class="w-100 d-flex justify-content-center">
+{{-- <div class="w-100 d-flex justify-content-center">
     {{ $products->links() }}
-</div>
+</div> --}}
